e51dc5c91a81aa1ad96e9e6152f3a4503e59fb68
paupb <paulitapb@live.com.ar>
2023-12-29 00:37:53 -0300

    add vercel build conf

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI);
 
