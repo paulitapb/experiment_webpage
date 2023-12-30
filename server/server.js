5cfe3883b4c7843dcb5e26826e4ab7a68db48aad
paupb <paulitapb@live.com.ar>
2023-12-30 01:45:43 -0300

    add dotenv

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+require('dotenv').config()
 const apiRoutes = require('./routes/api');
 
 const app = express();
