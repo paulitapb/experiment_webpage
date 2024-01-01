6df3629a126372675f932e2b16e7154cd0e2365f
paupb <paulitapb@live.com.ar>
2024-01-01 19:21:38 -0300

    add vercel conf

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
+const http  = require("http");
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config(); //{ path: '/.env' }
 
 
-app.use(cors({
-  origin: 'https://experimentsd-o0d1lusk2-paulas-projects-16fd8cb8.vercel.app'
-}));
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
@@ -17,14 +16,15 @@ const connectDB = async () => {
       console.log("Connect to MongoDB successfully")
   } catch (error) {
       console.log("Connect failed " + error.message )
+      process.exit(1);
   }
 }
 
+app.use(cors({
+  origin: 'https://experimentsd-o0d1lusk2-paulas-projects-16fd8cb8.vercel.app'
+}));
 
 const apiRoutes = require('./routes/api');
-
-
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', apiRoutes);
@@ -33,5 +33,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+
+const server = http.createServer(app);
 
 connectDB();
