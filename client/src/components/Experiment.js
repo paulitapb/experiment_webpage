cd9d734bbea8f13c2c1a7a6adaf962345f076536
paupb <paulitapb@live.com.ar>
2024-01-11 01:59:41 -0300

    fix bug

--- client/src/components/Experiment.js
+++ client/src/components/Experiment.js
@@ -12,7 +12,7 @@ const generateRandomIndices = async (userId) => {
   const randomIndices = new Set();
   while (randomIndices.size < imgAmountToRate) {
     const randomIndex = Math.floor(Math.random() * images.length);
-    const userAlreadyRated = await axios.get('https://experiment-webpage-server.vercel.app/api/hasRated', {
+    const userAlreadyRated = await axios.post('https://experiment-webpage-server.vercel.app/api/hasRated', {
       params: {
         userId: userId,
         imgId: randomIndex
