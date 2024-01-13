cbad25800023927493e75fd1067078d2468525cf
paupb <paulitapb@live.com.ar>
2024-01-13 17:14:25 -0300

    try to fix cors conf

--- server/db.js
+++ server/db.js
@@ -9,7 +9,7 @@ const allowCORS = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://experimentsd-web.vercel.app'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
