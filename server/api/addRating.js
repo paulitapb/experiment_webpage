28653fcbbd82c4164f1d9c31cca2e7b0c8c75637
paupb <paulitapb@live.com.ar>
2024-01-13 19:51:19 -0300

    fix addRating

--- server/api/addRating.js
+++ server/api/addRating.js
@@ -3,19 +3,18 @@ const { mongoose, allowCORS } = require('../db');
 
 const ExperimentModel = require('../models/ExperimentModel');
 
-
 module.exports = async (req, res) => {
   try {
     allowCORS(req, res, () => {});
-    
+
     const {userId, imgId, imgGroup, imgGeneratedBy, promptUsed, rating } = req.body; 
     
     if (!imgId || !imgGroup || !promptUsed || !rating) {
       return res.status(400).json({ error: 'Missing required fields' });
     }
   
-    
-      let user = await ExperimentModel.findOne({ userId: String(userId) });
+      console.log("user id" + userId)
+      let user = await ExperimentModel.findOne({ userId: userId });
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
