4029f642f72b59c4ec44b7789ad70e022479001e
paupb <paulitapb@live.com.ar>
2024-01-13 12:40:53 -0300

    fix cors

--- server/db.js
+++ server/db.js
@@ -5,14 +5,11 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Database connected'))
   .catch(err => console.error('Database connection error', err));
 
-  const allowCORS = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //TODO change this to the web domain
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    if (req.method === 'OPTIONS') {
-      return res.status(200).end();
-    }
-    next();
-  };
+const allowCORS = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://experimentsd-web.vercel.app/'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
   
 module.exports = { mongoose, allowCORS };
