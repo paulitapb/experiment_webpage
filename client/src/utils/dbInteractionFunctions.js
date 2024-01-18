5b8b59125fe89e6f649bdaa1ba59aee5a51da9c5
paupb <paulitapb@live.com.ar>
2024-01-18 13:39:57 -0300

    add logs

--- client/src/utils/dbInteractionFunctions.js
+++ client/src/utils/dbInteractionFunctions.js
@@ -7,6 +7,8 @@ export async function checkUserExists(hashedCellNumber){
       {
         userId: hashedCellNumber,
       });
+      console.log(response)
+      console.log(response.data.userExists);
       return (response.data.userExists);
      
     } catch (error) {
