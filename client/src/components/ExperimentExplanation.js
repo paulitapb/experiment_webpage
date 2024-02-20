4175bc8864ac8f35327b7335388524b351cc8123
paupb <paulitapb@live.com.ar>
2024-02-20 15:20:00 -0300

    fix img size

--- client/src/components/ExperimentExplanation.js
+++ client/src/components/ExperimentExplanation.js
@@ -8,8 +8,8 @@ function ExperimentExplanation(){
         <p className='experiment-explanation'>
           En un experimento anterior le mostramos a distintas personas imágenes y les pedimos que las describieran en texto. Luego otras personas generaron imágenes a partir de las descripciones y se evaluó cuán parecidas eran esas imágenes a las originales. Ahora le dimos estas descripciones a distintas Inteligencias Artificiales y queremos ver si generan imágenes parecidas a las originales. 
         </p>
-        <div>
-          <img src={experimentDiagram} className='image-container-home' alt="Diagrama del experimento" ></img>
+        <div className='image-container-home'>
+          <img src={experimentDiagram}  alt="Diagrama del experimento" ></img>
         </div>
       </div>
     );
