bd828f9e467ac6775ee62b5744b87f82672d356d
paupb <paulitapb@live.com.ar>
2024-02-19 21:30:47 -0300

    fix server

--- server/api/getCurrentSerie.js
+++ server/api/getCurrentSerie.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
     
     const current_serie_index = await CurrentSerieIndex.findOne();
     if (!current_serie_index) {
-      console.log('No document found with findOne');
+      res.status(404).json({ message: 'Document not found' });
       return;
     }
     
@@ -19,9 +19,9 @@ module.exports = async (req, res) => {
       { _id: current_serie_index._id }, 
       { $set: { currentIndex: new_index } },
       { new: true }  // return the updated document
-    );
+    ).catch(err => { console.error(err); });
 
-    res.json(updated_serie_index);
+    res.json(current_serie_index.currentIndex);
 
   } catch (err) {
     console.error(err);
