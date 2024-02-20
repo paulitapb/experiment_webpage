2675d85730354735d8ea12edf8a39644c7ef7049
paupb <paulitapb@live.com.ar>
2024-02-20 20:44:23 -0300

    change time warning location

--- client/src/components/ExperimentExplanation.js
+++ client/src/components/ExperimentExplanation.js
@@ -8,12 +8,12 @@ function ExperimentExplanation(){
         <p className='experiment-explanation'>
           En un experimento anterior le mostramos a distintas personas imágenes y les pedimos que las describieran en texto. Luego otras personas generaron imágenes a partir de las descripciones y se evaluó cuán parecidas eran esas imágenes a las originales. Ahora le dimos estas descripciones a distintas Inteligencias Artificiales y queremos evaluar cuán bien salió ese proceso.
         </p>
-        <p className='Center'>
-          Podes realizar este experimento desde cualquier dispositivo y te llevará 15 minutos completarlo.
-        </p>
         <div className='image-container-home'>
           <img src={experimentDiagram}  alt="Diagrama del experimento" ></img>
         </div>
+        <p className='Center'>
+          Podes realizar este experimento desde cualquier dispositivo y te llevará 15 minutos completarlo.
+        </p>
       </div>
     );
   }; 
