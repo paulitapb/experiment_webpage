46c94693e546a9d4cfcad7e67891c2003995d36e
paupb <paulitapb@live.com.ar>
2024-02-21 11:25:46 -0300

    fix rating not aligned

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -37,7 +37,7 @@ export default class FiveStarsRating extends Component {
 
     render() {
       return [...Array(5).keys()].map((index) => (
-        <span key={index} style={{ color: this.starColor(index) }}>
+        <span key={index} style={{ color: this.starColor(index), flexDirection: 'row' }}>
           <FontAwesomeIcon
             icon={faStar}
             style={{ height: '40px', cursor: 'pointer' }}
