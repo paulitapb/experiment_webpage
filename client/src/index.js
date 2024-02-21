d235351bfdb869d10aff17d2c676d5c5b9c59c8a
paupb <paulitapb@live.com.ar>
2024-02-21 12:54:39 -0300

    delete resize variable

--- client/src/index.js
+++ client/src/index.js
@@ -11,11 +11,6 @@ import TutorialCompareImages from './pages/TutorialCompareImages.js';
 import './styles/global.css';
 import './index.css';
 
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-window.dispatchEvent(new Event('resize'));
 
 
 const router = createBrowserRouter([
